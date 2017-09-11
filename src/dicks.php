09849e30a10620376ca6c56581ccdd2b7b8dda2e
Dickson <languageusa@163.com>
2017-09-11 16:13:45 +0800

    -- test commit

---
+++ src/dicks.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *-----------------------------------------------------
+ * Created by PhpStorm.
+ *-----------------------------------------------------
+ *
+ * Date: 2017/9/11 16:12
+ * @author : Dicks
+ * @email  : languageusa@163.com
+ */
+namespace dicks;
+
+class Test
+{
+    public function Hello()
+    {
+        echo 'Hello';
+    }
+}
