d0e47cdf75bf0ce7980b1621f275e08eaae472c7
Dickson <languageusa@163.com>
2017-09-11 16:40:12 +0800

    -- test commit

--- src/Test.php
+++ src/Test.php
@@ -4,11 +4,13 @@
  * Created by PhpStorm.
  *-----------------------------------------------------
  *
- * Date: 2017/9/11 16:12
+ * Date: 2017/9/11 16:39
  * @author : Dicks
  * @email  : languageusa@163.com
  */
+
 namespace dicks;
+
 
 class Test
 {
